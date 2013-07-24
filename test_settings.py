dc9469597340f61b1d0a1afb10d73801f303de7b
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2013-07-24 23:14:18 +0200

    Work on some of the issues...
    Some tests test for hardcoded urls, I tried to change them
    (with no effect).
    
    Particularly: around object_permissions.tests.groups:1200

--- test_settings.py
+++ test_settings.py
@@ -4,10 +4,30 @@ DATABASES = {
     }
 }
 
+SITE_ID = 303
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SECRET_KEY = "abc"
+
+DATABASES = {"default": {
+    "NAME": ":memory:",
+    "ENGINE": "django.db.backends.sqlite3",
+    "USER": '',
+    "PASSWORD": '',
+    "PORT": '',
+}}
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'object_permissions',
 )
 
-TESTING = True
+TEMPLATE_DIRS = (
+    "test_templates",
+)
+
+TESTING = DEBUG
+
+ROOT_URLCONF = 'test_urls'

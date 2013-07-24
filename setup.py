cdf386a7500d1a0f505c6c33fb95467897cfca95
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2013-07-24 12:03:52 +0200

    New: Travis-ci love

--- setup.py
+++ setup.py
@@ -8,16 +8,27 @@ setup(name='django-object-permissions',
       version="1.4.6",
       description='A method for adding object-level or row-level permissions',
       long_description=long_desc,
+
       author='Peter Krenesky',
       author_email='kreneskyp@osuosl.org',
       maintainer="Ken Lett",
       maintainer_email="kennric@osuosl.org",
       url='http://code.osuosl.org/projects/object-permissions',
+
       packages=['object_permissions'],
       include_package_data=True,
+
       classifiers=[
-          "License :: OSI Approved :: MIT License",
-          'Framework :: Django',
-          'Topic :: Security',
-          ],
-      )
+                  "License :: OSI Approved :: MIT License",
+                  "Framework :: Django",
+                  "Topic :: Security",
+      ],
+
+      # Enable django-setuptest
+      test_suite='setuptest.setuptest.SetupTestSuite',
+      tests_require=(
+        'django-setuptest',
+        # Required by django-setuptools on Python 2.6
+        'argparse'
+      ),
+)
---
+++ test_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'object_permissions',
+)
+
+TESTING = True
